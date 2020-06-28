2940fd3dd6e9b85d0dd97cac11c32e9b51b4b10b
@thar <tharnnn1401@gmail.com>
2020-06-28 22:22:33 +0700

    28.06.2020

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <div class="container h-100">
         <div>
           <nav class="navbar transparent navbar-inverse">
-            <router-link to="/" class="navbar-brand h1 mb-0" href="#">SMpaking</router-link>
+            <router-link to="/" class="navbar-brand h1 mb-0" href="#">SMparking</router-link>
             <div class="nav-item">
               <button
                 class="btn btn-primary"
@@ -17,8 +17,9 @@
           </nav>
 
           <div class="row justify-content-md-center">
+            
             <div class="col-4">
-              <b-card
+              <!-- <b-card
                 title="1st Floor"
                 img-alt="Image"
                 tag="article"
@@ -26,8 +27,8 @@
                 class="m-3"
               >
                 <b-card-text class="floor-num">Available 100</b-card-text>
-              </b-card>
-              <b-card
+              </b-card> -->
+              <!-- <b-card
                 title="2nd Floor"
                 img-alt="Image"
                 tag="article"
@@ -35,9 +36,9 @@
                 class="m-3"
               >
                 <b-card-text class="floor-num">Available 100</b-card-text>
-              </b-card>
+              </b-card> -->
 
-              <b-card
+              <!-- <b-card
                 title="3rd Floor"
                 img-alt="Image"
                 tag="article"
@@ -45,30 +46,26 @@
                 class="m-3"
               >
                 <b-card-text class="floor-num">Available 150</b-card-text>
-              </b-card>
+              </b-card> -->
 
-              <b-card
-                title="4th Floor"
+              
+              <ul style="list-style:none;">
+                <li v-for="(value, key) in map" :key="key" :prop="value">
+                <b-card
+                :title= "'Level ' + [[ key+1 ]]  "
                 img-alt="Image"
                 tag="article"
                 style="max-width: 20rem;"
                 class="m-3"
-              >
-                <b-card-text class="floor-num">Available 150</b-card-text>
-              </b-card>
+                >
+                <b-card-text class="floor-num">{{ value[1] }}</b-card-text>
+                </b-card>
+                </li>
+              </ul>
+
             </div>
 
             <div class="col-6">
-              <b-card
-                title="Recommend"
-                img-alt="Image"
-                tag="article"
-                style="max-width: 20rem;"
-                class="m-3"
-              >
-                <b-card-text class="recommend-font">1A-01</b-card-text>
-              </b-card>
-
               <b-card
                 title="Available Total"
                 img-alt="Image"
@@ -76,8 +73,19 @@
                 style="max-width: 20rem;"
                 class="m-3"
               >
-                <b-card-text class="floor-num">150</b-card-text>
+                <b-card-text class="recommend-font">{{countTotal}}</b-card-text>
               </b-card>
+
+              <!-- <b-card
+                title="Available Total"
+                img-alt="Image"
+                tag="article"
+                style="max-width: 20rem;"
+                class="m-3"
+              >
+                <b-card-text class="floor-num">150</b-card-text>
+              </b-card> -->
+
             </div>
           </div>
           <div>
@@ -87,8 +95,19 @@
               title="Config"
               class="btn btn-info btn-lg m-2"
             >To Config</router-link>
+
+            <button @click="getZoneWithSlot()">count</button>
+
             <button class="btn btn-danger btn-lg" @click="resetDB('floors')">Reset DB</button>
           </div>
+          <!-- <div>
+            <router-link
+              to="/config"
+              tag="button"
+              title="Config"
+              class="btn btn-danger btn-lg m-2"
+            >To Dashboard</router-link>
+          </div>-->
         </div>
       </div>
     </div>
@@ -99,25 +118,140 @@
 
 <script>
 import Login from "@/components/Login.vue";
-import { db } from "../firebase";
+import { db, rdb } from "../firebase";
+import * as firebase from "firebase";
+
 export default {
   name: "Home",
   components: { Login },
 
+  data() {
+    return {
+      countTotal: null,
+      arrFloorcount : [],
+      arrfloor : [],
+      map : new Map(),
+    };
+  },
+
   methods: {
-    resetDB() {
-      console.log("reset");
+    // resetDB() {
+    //   console.log("reset");
+    //   db.collection("floors")
+    //     .get()
+    //     .then(res => {
+    //       res.forEach(element => {
+    //         element.ref.delete();
+    //       });
+    //     });
+    // }
+
+    getZoneWithSlot() {
+     
+      let floor = 0;
+      let floorcount = 0;
+      // let map = new Map();
+      // let arrFloor = [];
+      
       db.collection("floors")
         .get()
-        .then(res => {
-          res.forEach(element => {
-            element.ref.delete();
+        .then(dataFloor => {
+          dataFloor.forEach(docFloor => {
+            // let size = data.size;
+            db.collection("floors")
+              .doc(docFloor.id)
+              .collection("zoneDetail")
+              .get()
+              .then(dataZone => {
+                // this.total = dataZone.size;
+                // console.log("counttotall222",this.total)
+
+                dataZone.forEach(docZone => {
+                  //console.log('floor',docFloor.id,'zone',docZone.id,"slot>>>>>>>>>>>", dataZone.size);
+
+                  db.collection("floors")
+                    .doc(docFloor.id)
+                    .collection("zoneDetail")
+                    .doc(docZone.id)
+                    .collection("slotDetail")
+                    .get()
+                    .then(dataSlot => {
+                      
+                      console.log(
+                        "floor>>>",
+                        docFloor.id,
+                        "zone>>>",
+                        dataZone.size,
+                        "slot>>>>>>>>>>>",
+                        dataSlot.size
+                      );
+                      this.countTotal += dataSlot.size;
+                      console.log("counttotall>>",this.countTotal)  
+                      if(!this.map.has(docFloor.id)){
+                        this.map.set(docFloor.id, dataSlot.size)
+                      }
+                      else{
+                        floorcount = this.map.get(docFloor.id)
+                        this.map.set(docFloor.id, floorcount+dataSlot.size)
+                      }
+                      console.log("floorrr", this.map)
+                      // floorcount += dataSlot.size;
+                      //   console.log("floorcount>>",floorcount)
+                      // floorcount = dataSlot.size*dataZone.size;
+                      // console.log("floorcount>>",floorcount)
+                      // this.arrFloorcount.push(floorcount)
+                      // console.log("floorcountarrayyy", this.arrFloorcount)
+                                          
+                      // dataSlot.forEach(docSlot => {
+                      //   console.log("slot>>>>>>>>>>>", dataSlot.size);
+                      // }); 
+                      
+                    })
+                    
+                        
+                    // console.log("counttotal", this.countTotal)
+                    //  this.total += dataZone.size;
+                    //  console.log("counttotall222",this.total)
+                     
+                    // .then(() => {
+                    //    floorCount = this.countTotal;
+                    //    if( docFloor.id==1){
+                    //      if(floorCount>max){
+                    //      max=floorCount;
+                    //      arrFloor.push(max);
+                    //      }
+                    //    }
+                       
+                       
+                    //    console.log("floorcount",floorCount);
+                       
+                      //  this.arrFloor.push(floorCount); 
+                      // .then(() => {
+                      //   floorcount += dataSlot.size;
+                      //   console.log("floorcount>>",floorcount)
+                      // })
+                                   
+                     
+                    })
+               
+                               
+              })
+                
           });
+
+          
         });
+        
+     
+     
     }
+  },
+  computed: {
+      
   }
 };
 </script>
+
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Raleway&display=swap");
 .home {
